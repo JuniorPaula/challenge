3706fa22f31b0c16381bcc3def8d50b47dad8c13
Junior Paula <luke.junnior@icloud.com>
2023-02-01 14:30:27 -0300

    Implemens states json database

--- states-api/routes/states-routes.js
+++ states-api/routes/states-routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express'
 const statesRouter = Router()
 
 statesRouter.get('/', async (req, res) => {
-    return res.end('hello states')
+  return res.end('hello states')
 })
 
 export { statesRouter }
