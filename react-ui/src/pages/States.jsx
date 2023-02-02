c4eeea507144cdb2de7876d398467d0ac9f99a04
Junior Paula <luke.junnior@icloud.com>
2023-02-02 10:14:22 -0300

    Implements page of states

--- react-ui/src/pages/States.jsx
+++ react-ui/src/pages/States.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import axios from 'axios'
 
 const States = () => {
+  const [states, setStates] = React.useState([])
+
+  const getStates = async () => {
+    try {
+      const response = await axios.get('http://localhost:4000/states/load')
+      const data = response.data
+
+      setStates(data)
+    } catch (error) {
+      console.log('[ERROR]:', error)
+    }
+  }
+
+  React.useEffect(() => {
+    getStates()
+  },[])
+
   return (
     <React.Fragment>
       <section>
-        <div className='centered text-4x1 h-screen'>lista de estados</div>
+        <div className='states-container'>
+          <h2 className='states-title'>Lista de estados</h2>
+          {states.length === 0 ? <p>Carregando...</p> : (
+          states.map((state, index) => (
+            <ul key={index}>
+              <li className='states-list'>{state.name}</li>
+            </ul>
+          ))
+          )}
+        </div>
       </section>
     </React.Fragment>
   )
